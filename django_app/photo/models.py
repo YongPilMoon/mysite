355c5e5222a740532ab38dc832eb96ca5fa3839d
YongPilMoon <m71661@gmail.com>
2016-10-25 16:33:20 +0900

    Add models fileds

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from member.models import MyUser
 
-# Create your models here.
+
+class Album(models.Model):
+    title = models.CharField(max_length=40)
+    owner = models.ForeignKey(MyUser)
+    description = models.CharField(max_length=80, blank=True)
+
+
+class Photo(models.Model):
+    Album = models.ForeignKey(Album)
+    owner = models.ForeignKey(MyUser)
+    title = models.CharField(max_length=40)
+    description = models.CharField(max_length=80, blank=True)
+    img = models.ImageField(upload_to='photo')
+
+
